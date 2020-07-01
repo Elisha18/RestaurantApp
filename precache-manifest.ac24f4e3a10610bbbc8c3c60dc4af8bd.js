d27a890ab6b3f3498f3ef5027005865eed84d7be
elisha18 <imelishageorge@gmail.com>
2020-07-02 00:03:37 +0530

    Updates

---
+++ precache-manifest.ac24f4e3a10610bbbc8c3c60dc4af8bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d4808dd334f3e4ad3870223957b2bc",
+    "url": "/Elisha18/RestaurantApp.git/index.html"
+  },
+  {
+    "revision": "60c13bb22039cee664c9",
+    "url": "/Elisha18/RestaurantApp.git/static/css/2.5b1a0ff0.chunk.css"
+  },
+  {
+    "revision": "c42ff474145117bac186",
+    "url": "/Elisha18/RestaurantApp.git/static/css/main.fc683296.chunk.css"
+  },
+  {
+    "revision": "60c13bb22039cee664c9",
+    "url": "/Elisha18/RestaurantApp.git/static/js/2.b1309613.chunk.js"
+  },
+  {
+    "revision": "3e9731b2b085080fa6585f45dbac918a",
+    "url": "/Elisha18/RestaurantApp.git/static/js/2.b1309613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42ff474145117bac186",
+    "url": "/Elisha18/RestaurantApp.git/static/js/main.7944959d.chunk.js"
+  },
+  {
+    "revision": "5c3f890500e2c57ce5e8",
+    "url": "/Elisha18/RestaurantApp.git/static/js/runtime-main.56060389.js"
+  },
+  {
+    "revision": "b5066171d35d0c6dcb52dcb87410fabe",
+    "url": "/Elisha18/RestaurantApp.git/static/media/card_1.b5066171.jpg"
+  },
+  {
+    "revision": "4cd6b9f976da720b87c68cfdd895e442",
+    "url": "/Elisha18/RestaurantApp.git/static/media/card_2.4cd6b9f9.jpg"
+  },
+  {
+    "revision": "9eef2f2fd1162f5fa346ac41e112c0ec",
+    "url": "/Elisha18/RestaurantApp.git/static/media/card_3.9eef2f2f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Elisha18/RestaurantApp.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Elisha18/RestaurantApp.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Elisha18/RestaurantApp.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Elisha18/RestaurantApp.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Elisha18/RestaurantApp.git/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
